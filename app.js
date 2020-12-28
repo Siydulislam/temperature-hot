c00e72d507c8c39cf75164255a4c172eeb390d23
Siydulislam <siydulislam333@gmail.com>
2020-12-28 12:08:26 +0600

    added new

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const getWeatherData = city => {
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        //console.log(data);
+        console.log(data);
         updateUI(data);
     })
 }
@@ -21,6 +21,7 @@ searchBtn.addEventListener("click", () => {
 const updateUI = data => {
     document.getElementById("show-city").innerText = data.name || "Unknown Location!";
     document.getElementById("show-temperature").innerText = data.main.temp;
+    document.getElementById("show-humidity").innerText = data.main.humidity;
     document.getElementById("weather-status").innerText = data.weather[0].main;
     document.getElementById("icon").setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
     document.getElementById("city").value = "";
